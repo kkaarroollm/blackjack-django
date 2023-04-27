4e0563d7dd7eb4e382fdb8200bf35a30b4974ab7
karol marszalek <mkarol.4514@gmail.com>
2023-04-27 23:51:58 +0200

    some routing.py and urls in bj app - for WebSocket

--- bj/routing.py
+++ bj/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
+
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server/', consumers.BjConsumer.as_asgi())
+    re_path(r'ws/blackjack/', consumers.BlackjackGameConsumer.as_asgi()),
 ]
